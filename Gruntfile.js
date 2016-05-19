12d9e40fd2ddf41571e58807f85c8b4858e911fb
Jonathan Kew <jfkthame@gmail.com>
2016-05-19 18:13:23 +0100

    Simple Gruntfile for building the raw .ttf font from SVGs

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+/*jshint node:true*/
+
+var path = require('path');
+
+module.exports = function(grunt) {
+	'use strict';
+
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+		webfont: {
+            EmojiOne: {
+                src: 'build/glyphs/*.svg',
+                dest: 'build/raw-font',
+                options: {
+                    font: 'EmojiOne',
+                    types: 'ttf',
+					codepointsFile: 'build/codepoints.js'
+                }
+            },
+		},
+	});
+
+	grunt.loadTasks('grunt-webfont/tasks');
+
+	grunt.registerTask('default', ['webfont']);
+};
