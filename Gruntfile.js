3ec72a6eb673787bf191d2fedcb8cc9ddc47c100
Essem <smswessem@gmail.com>
2017-11-30 09:35:03 -0600

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
 		},
 	});
 
-	grunt.loadTasks('grunt-webfont/tasks');
+	grunt.loadNpmTasks('grunt-webfont');
 
 	grunt.registerTask('default', ['webfont']);
 };
