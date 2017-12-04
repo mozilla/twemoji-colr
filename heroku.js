193a2f72e9c721e88f500cb09e71066569cbef99
Essem <smswessem@gmail.com>
2017-12-04 10:19:10 -0600

    Update heroku.js

--- heroku.js
+++ heroku.js
@@ -5,8 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 
 //For avoidong Heroku $PORT error
 app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
+    response.sendfile(__dirname + '/tests' + '/index.html');
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
