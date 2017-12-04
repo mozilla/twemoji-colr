81368e024b455ff415e9f9c3bf9bfa4aca67e154
Essem <smswessem@gmail.com>
2017-12-04 09:52:36 -0600

    Update heroku.js

--- heroku.js
+++ heroku.js
@@ -1,3 +1,48 @@
-var http = require('http');
+ var http = require('http');
+ var fs = require('fs');
+ var path = require('path');
 
-http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); }).listen(process.env.PORT || 5000);
+ http.createServer(function (request, response) {
+
+    console.log('request starting for ');
+    console.log(request);
+
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    console.log(filePath);
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+
+    path.exists(filePath, function(exists) {
+
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+
+ }).listen(process.env.PORT || 5000);
+
+ console.log('Server running');
