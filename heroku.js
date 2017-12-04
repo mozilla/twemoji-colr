d8a7f7f4148676277fe9db936a4f6f4e7dfa0eb2
Essem <smswessem@gmail.com>
2017-12-04 08:48:32 -0600

    Update heroku.js

--- heroku.js
+++ heroku.js
@@ -1,3 +1,3 @@
 var http = require('http');
 
-http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
+http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); }).listen(process.env.PORT || 5000);
