0d2d0fc26136d562a32b1577044017a960f839ff
TheEssemCraft <smswessem@gmail.com>
2017-12-05 15:37:53 -0600

    testing things out

--- fixDirection.py
+++ fixDirection.py
@@ -1,13 +1,14 @@
 import fontforge
 import sys
+import psMat
 
 fontfile = sys.argv[1]
 
 f = fontforge.open(fontfile)
-
-print(fontfile)
+identity = psMat.identity
+matrix = psMat.inverse(identity)
 
 for glyph in f.glyphs():
-    glyph.correctDirection()
+    glyph.transform(matrix)
 
 f.generate(fontfile)
