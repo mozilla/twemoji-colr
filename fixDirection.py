4abe10261949a1907dbeebd6a6eef9207e81bb8a
Essem <smswessem@gmail.com>
2017-12-07 13:00:13 -0600

    Update fixDirection.py

--- fixDirection.py
+++ fixDirection.py
@@ -6,7 +6,7 @@ import math
 fontfile = sys.argv[1]
 
 f = fontforge.open(fontfile)
-flip = psMat.scale(1.5,-1.5)
+flip = psMat.scale(1.25,-1.25)
 align = psMat.translate(0,600)
 
 for glyph in f.glyphs():
