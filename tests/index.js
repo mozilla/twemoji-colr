6c70ef8cc485bd4c6a1636f7e3c953471394d027
Essem <smswessem@gmail.com>
2018-02-26 19:57:16 -0600

    Update index.js

--- tests/index.js
+++ tests/index.js
@@ -218,7 +218,8 @@ ComparisonTest.prototype = {
     var svgUrl = this.svgUrl = '../build/colorGlyphs/u' +
       this.codePoints.filter(function(cp) {
         // Remove zero width joiner and VS16.
-        return /*cp !== 0x200d &&*/ cp !== 0xfe0f;
+        console.log(cp);
+        return cp !== 0x200d && cp !== 0xfe0f;
       })
       .map(function(cp) {
         var str = cp.toString(16);
